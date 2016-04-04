fd81f59049b16d56a1c46ae08a84b4d3ec35091d
jiclotus <jose-castelli@hotmail.com>
2016-04-04 16:06:34 -0300

    Mejoro dibujado de esfera, falta corregir un error en los angulos para q dibuje correctamente.

--- Objetos/Esfera.js
+++ Objetos/Esfera.js
@@ -2,7 +2,7 @@
             function EsferaGrid (_radio) {
 
 						this.radio = _radio;
-						this.grilla = new VertexGrid(5,5);
+						this.grilla = new VertexGrid(15,15);
 
                this.createUniformEsfera = function(){
                     
@@ -10,18 +10,27 @@
                     this.grilla.color_buffer = [];
 
                     var cte=((this.grilla.cols-1.0)/4.0); 
-                    var valor1=0.0;
-                    var valor2=0.0;
+                    var x=0.0;
+                    var y=0.0;
+                    var z=0.0;
+                    var phi=0.0;
+                    var thita=0.0;
                      
                     for (var j=0;j<this.grilla.cols;j++){
                     		for (var i=0;i<this.grilla.rows;i++){
   									
-  									valor1=j-cte;
-  									valor2=i-cte;
+  									//x=j-cte;
+  									//y=i-cte;
+  									thita+=400.0;
+  									phi+=400.0;
+									
+  									x = this.radio * Math.cos(phi) * Math.cos(thita);
+  									y = this.radio * Math.cos(phi) * Math.sin(thita);
+  									z = this.radio * Math.sin(phi);
   									
-  									this.grilla.position_buffer.push(valor1);
-									this.grilla.position_buffer.push(valor2);
-									this.grilla.position_buffer.push(0.0);		
+  									this.grilla.position_buffer.push(x);								
+									this.grilla.position_buffer.push(y);
+									this.grilla.position_buffer.push(z);		
   
 		  							//Todos los vertices siempre blanco
       		              	this.grilla.color_buffer.push(1.0);
